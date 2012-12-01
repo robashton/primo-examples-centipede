fe3811636cf793bb156e4af6fe13a1864e09cadf
Rob Ashton <robashton@codeofrob.com>
2012-12-01 12:38:59 +0000

    Refactored to toolbar

--- site/editor/main.js
+++ site/editor/main.js
@@ -1,61 +1,23 @@
 define(function(require) {
-  var $ = require('jquery')
   var Eventable = require('eventable')
   var Runner = require('../engine/core')
-  var _ = require('underscore')
-  require('bootstrap')
-
+  var Toolbar = require('./toolbar')
   
   var Editor = function(targetid) {
-    $('.dropdown-toggle').dropdown()
     Eventable.call(this)
     this.targetid = targetid
     this.level = null
-    this.levels = null
-    this.entities = null
-    this.$controls = $('#controls')
-    this.$listoflevels = $('#list-of-levels')
     this.engine = new Runner(targetid)
-    $.getJSON('/levels', _.bind(this.onLevelsReceived, this))
-    $.getJSON('/entities', _.bind(this.onEntitiesReceived, this))
-    this.$listoflevels.on('click a', _.bind(this.onLevelSelected,this))
+    this.toolbar = new Toolbar()
+    this.toolbar.on('level-loaded', _.bind(this.onLevelLoaded, this))
     setInterval(_.bind(this.render, this), 500)
   }
   Editor.prototype = {
-    onEntitiesReceived: function(entities) {
-      this.entities = entities
-      this.tryStart()
-    },
-    onLevelsReceived: function(levels) {
-      this.levels = levels
-      this.tryStart()
-    },
-    tryStart: function() {
-      if(this.levels && this.entities)
-        this.start()
-    },
-    start: function() {
-      for(var i = 0 ; i < this.levels.length; i++) {
-        var level = this.levels[i]
-        this.$listoflevels.append(
-          $('<li/>').html(
-            $('<a/>')
-              .attr('href', '#')
-              .text(level.name)
-              .data('level', level)
-          )
-        )
-      }
-    },
-    onLevelSelected: function(e) {
-      var level = $(e.target).data('level')
-      var self = this
-      require([level.link], function(level) {
-        self.engine.loadLevel(level)
-      })
-    },
     render: function() {
       this.engine.render()
+    },
+    onLevelLoaded: function(level) {
+      this.engine.loadLevel(level)
     }
   }
   _.extend(Editor.prototype, Eventable.prototype)
---
+++ site/editor/toolbar.js
@@ -0,0 +1,54 @@
+define(function(require) {
+  var $ = require('jquery')
+  var Eventable = require('eventable')
+  var _ = require('underscore')
+  require('bootstrap')
+
+  var Toolbar = function() {
+    Eventable.call(this)
+    $('.dropdown-toggle').dropdown()
+    this.$listoflevels = $('#list-of-levels')
+    this.$listoflevels.on('click a', _.bind(this.onLevelSelected,this))
+    this.levels = null
+    this.entities = null
+    $.getJSON('/levels', _.bind(this.onLevelsReceived, this))
+    $.getJSON('/entities', _.bind(this.onEntitiesReceived, this))
+  }
+
+  Toolbar.prototype = {
+    onEntitiesReceived: function(entities) {
+      this.entities = entities
+      this.tryStart()
+    },
+    onLevelsReceived: function(levels) {
+      this.levels = levels
+      this.tryStart()
+    },
+    tryStart: function() {
+      if(this.levels && this.entities)
+        this.start()
+    },
+    start: function() {
+      for(var i = 0 ; i < this.levels.length; i++) {
+        var level = this.levels[i]
+        this.$listoflevels.append(
+          $('<li/>').html(
+            $('<a/>')
+              .attr('href', '#')
+              .text(level.name)
+              .data('level', level)
+          )
+        )
+      }
+    },
+    onLevelSelected: function(e) {
+      var level = $(e.target).data('level')
+      var self = this
+      require([level.link], function(level) {
+        self.raise('level-loaded', level)
+      })
+    }
+  }
+  _.extend(Toolbar.prototype, Eventable.prototype)
+  return Toolbar
+})
