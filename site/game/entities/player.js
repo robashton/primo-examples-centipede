ca2dd852288e994964f1397c2c6879f355426830
Rob Ashton <robashton@codeofrob.com>
2013-01-06 15:00:27 +0000

    Rudimentary animation class added

--- site/game/entities/player.js
+++ site/game/entities/player.js
@@ -1,10 +1,10 @@
 define(function(require) {
   var Entity = require('engine/entity')
-  var ColouredQuad = require('../components/colouredquad')
-  var Controller = require('../components/controller')
+  var Animation = require('game/components/animation')
+  var Controller = require('game/components/controller')
 
   var Player = Entity.Define(function(id, data) {
-    this.attach(new ColouredQuad(this, '#F00'))
+    this.attach(new Animation(this, '/media/dancing.png', 16, 16, 15, [0,1]))
     this.attach(new Controller(this))
     this.width = 10
     this.height = 10
