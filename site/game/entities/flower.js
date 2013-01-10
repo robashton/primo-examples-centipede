77ff671f288df310b50361dd534fcbf73cab2128
Rob Ashton <robashton@codeofrob.com>
2013-01-10 18:11:53 +0000

    Removed pickup component, did it with head instead /game

--- site/game/entities/flower.js
+++ site/game/entities/flower.js
@@ -6,22 +6,8 @@ define(function(require) {
   return Entity.Define(function(id, data) {
     this.width = 6
     this.height = 6
-
     this.attach(new RigidBody(this, { weight: 0 }))
     this.attach(new Animation(this, 'media/flower.png', 6, 6 ))
       .define('idle', 5, [ 0, 1, 2, 3, 4 ])
-
-    /*
-    check: function(other) {
-      this.parent(other)
-      if(other instanceof EntityBullet) {
-        other.kill()
-      }
-      else if(other instanceof EntityCentipedeHead) {
-        other.grow()
-        this.kill()
-        Events.raise('flower-eaten', this)
-      }
-    } */
   })
 })
