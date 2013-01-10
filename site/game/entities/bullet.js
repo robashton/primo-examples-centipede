4214806ee7896b10659afe27f868007e8509236a
Rob Ashton <robashton@codeofrob.com>
2013-01-10 19:56:13 +0000

    Added the idea of a bullet component in core /game

--- site/game/entities/bullet.js
+++ site/game/entities/bullet.js
@@ -1,31 +1,16 @@
-ig.module(
-	'game.entities.bullet'
-)
-.requires(
-  'impact.entity',
-  "game.eventbus",
-  "game.eventbus",
-  "game.eventbus",
-  "game.eventbus"
-)
-.defines(function(){
+define(function(require) {
+  var Entity = require('engine/entity')
+  var Animation = require('engine/components/animation')
+  var Bullet = require('engine/components/bullet')
+  var util = require('engine/commons')
 
-  EntityBullet = ig.Entity.extend({
-    size: {x: 4, y: 4},
-    collides: ig.Entity.COLLIDES.LITE,
-    checkAgainst: ig.Entity.TYPE.A,
-    type: ig.Entity.TYPE.B,
-    animSheet: new ig.AnimationSheet('media/bullet.png', 4,4),
-    init: function( x, y, settings ) {
-      this.parent( x, y, settings );
-      this.addAnim( 'idle', 0.1, [0, 1, 2]);
-      this.vel.x = typeof settings.spread === 'undefined' ? 0 : settings.spread
-      this.vel.y = -settings.speed
-      Events.raise('bullet-fired')
-    },
-    check: function(other) {
-      this.parent(other)
-    }
+  return Entity.Define(function(id, data){
+    this.width = 4
+    this.height = 4
+    this.attach(new Bullet(this))
+    this.attach(new Animation(this, 'media/bullet.png', 4, 4))
+      .define('idle', 10, [0, 1, 2])
+    this.velx = util.valueOrDefault(data.spread, 0)
+    this.vely = -data.speed
   })
-
 })
