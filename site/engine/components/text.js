132e9ed6c702b3e49b50160cf6040d115aabd025
Rob Ashton <robashton@codeofrob.com>
2013-01-11 15:55:03 +0000

    coloured text thanks

--- site/engine/components/text.js
+++ site/engine/components/text.js
@@ -1,12 +1,14 @@
 define(function(require) {
 
-  var Text = function(entity, text) {
+  var Text = function(entity, text, colour) {
     this.entity = entity
     this.text = text
+    this.colour = colour || '#FFF'
   }
 
   Text.prototype = {
     render: function(context) {
+      context.fillStyle = this.colour
       context.font = this.entity.height + 'px sans-serif'
       context.fillText(this.text, this.entity.x, this.entity.y)
     }
