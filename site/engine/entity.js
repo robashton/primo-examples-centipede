54a1e7e2c10c37ba674e6ab24882f67056cd2321
Rob Ashton <robashton@codeofrob.com>
2013-01-11 16:05:25 +0000

    Naughty little prototype fuddling dEfine method

--- site/engine/entity.js
+++ site/engine/entity.js
@@ -85,12 +85,15 @@ define(function(require) {
     }
   }
 
-  Entity.Define = function(init) {
+  Entity.Define = function(init, methods) {
     var Ctor = function(id, data, game) {
       Entity.call(this, id, data, game)
       init.call(this, id, data)
     }
-    _.extend(Ctor.prototype, Entity.prototype)
+
+    methods = methods || []
+    methods.push(Entity.prototype)
+    _.extend(Ctor.prototype, methods)
     return Ctor
   }
   _.extend(Entity.prototype, Eventable.prototype)
