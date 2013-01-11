5a8e829ba54a33df1ae561f4830d099f8f5d3bbf
Rob Ashton <robashton@codeofrob.com>
2013-01-11 12:48:28 +0000

    Helps if you point at a valid resource /game

--- site/game/entities/deadsegment.js
+++ site/game/entities/deadsegment.js
@@ -7,7 +7,7 @@ define(function(require) {
     this.width = 8
     this.height = 8
     this.attach(new TimedRemoval(this, 1))
-    this.attach(new Animation(this, 'media/centipede', 8,8))
+    this.attach(new Animation(this, 'media/centipede.png', 8,8))
       .define('idle', 0.2, [10, 15, 16, 17, 18, 19])
   })
 
