42bb2906941b6443b06b3f37e29cffae4cd684af
Rob Ashton <robashton@codeofrob.com>
2013-01-14 13:25:34 +0000

    Removed AMD from components

--- site/components/shrinker.js
+++ site/components/shrinker.js
@@ -1,26 +1,25 @@
-define(function(require) {
-  var _ = require('underscore')
+var _ = require('underscore')
 
-  var Shrinker = function(entity, sizes) {
-    this.sizes = sizes
-    this.entity = entity
-    this.current = 0
-    this.entity.handle('hitbybullet', _.bind(this.hitByBullet, this))
-    this.updateEntity()
-  }
+var Shrinker = function(entity, sizes) {
+  this.sizes = sizes
+  this.entity = entity
+  this.current = 0
+  this.entity.handle('hitbybullet', _.bind(this.hitByBullet, this))
+  this.updateEntity()
+}
 
-  Shrinker.prototype = {
-    hitByBullet: function() {
-      this.current++
-      if(this.current === this.sizes.length)
-        return this.entity.kill()
-      else
-        this.updateEntity()
-    },
-    updateEntity: function() {
-      this.entity.width = this.sizes[this.current]
-      this.entity.height = this.sizes[this.current]
-    }
+Shrinker.prototype = {
+  hitByBullet: function() {
+    this.current++
+    if(this.current === this.sizes.length)
+      return this.entity.kill()
+    else
+      this.updateEntity()
+  },
+  updateEntity: function() {
+    this.entity.width = this.sizes[this.current]
+    this.entity.height = this.sizes[this.current]
   }
-  return Shrinker
-})
+}
+
+module.exports = Shrinker
--- site/components/timedremoval.js
+++ site/components/timedremoval.js
@@ -1,19 +1,15 @@
-define(function(require) {
+var TimedRemoval = function(entity, time) {
+  this.time = time
+  this.entity = entity
+  this.totalsofar = 0
+}
 
-  var TimedRemoval = function(entity, time) {
-    this.time = time
-    this.entity = entity
-    this.totalsofar = 0
+TimedRemoval.prototype = {
+  tick: function(frameTime) {
+    this.totalsofar += frameTime
+    if(this.totalsofar >= this.time)
+      this.entity.kill()
   }
+}
 
-  TimedRemoval.prototype = {
-    tick: function(frameTime) {
-      this.totalsofar += frameTime
-      if(this.totalsofar >= this.time)
-        this.entity.kill()
-    }
-  }
-
-  return TimedRemoval
-
-})
+module.exports = TimedRemoval
