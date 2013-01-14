e2e69d2a8ec145fa9f9010b905fa4966c8a09e5d
Rob Ashton <robashton@codeofrob.com>
2013-01-14 13:23:29 +0000

    Removed obsoletes from server

--- server.js
+++ server.js
@@ -2,50 +2,9 @@ var http = require('http');
 var send = require('send');
 var url = require('url');
 var path = require('path')
-var fs = require('fs')
 var app = require('express')()
-var _ = require('underscore')
 
-var server = http.createServer(app).listen(8080)
-app.use(require('express').bodyParser())
-
-app.put('/game/levels/:file', function(req, res) {
-   fs.writeFile('site/game/levels/' + req.params.file, JSON.stringify(req.body), 'utf8', function(err) {
-     if(err) {
-       res.statusCode = 500
-       res.end('' + err)
-     } else {
-       res.end('success')
-     }
-   })
-})
-
-app.get('/levels', function(req, res) {
-  fs.readdir('site/game/levels', function(err, files) {
-    var levels = _(files).map(function(file) {
-        return { name: file, link: '../game/levels/' + file }
-    })
-    res.send(levels)
-  })
-})
-
-app.get('/tilesets', function(req, res) {
-  fs.readdir('site/game/tilesets', function(err, files) {
-    var tilesets = _(files).map(function(file) {
-        return { name: file, link: '../game/tilesets/' + file }
-    })
-    res.send(tilesets)
-  })
-})
-
-app.get('/entities', function(req, res) {
-  fs.readdir('site/game/entities', function(err, files) {
-    var levels = _(files).map(function(file) {
-        return { name: file, link: '../game/entities/' + file.split('.')[0] }
-    })
-    res.send(levels)
-  })
-})
+var server = http.createServer(app).listen(process.env.PORT || 8080)
 
 app.use(function(req, res) {
   function error(err) {
@@ -63,5 +22,3 @@ app.use(function(req, res) {
     .on('directory', redirect)
     .pipe(res);
 })
-
-server.listen(8080);
